696b88bc88d672d8b95c9b982c968799465f458f
CHAKFI Ahmed <36175107+CHAKFI@users.noreply.github.com>
2018-03-06 22:18:12 +0000

    EXO
    kilo

---
+++ Ex5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+double P_HT,TVA,nbr_kilo,P_TTC;
+ 
+cout<<"Veuillez saisir le prix HT d'un kilo de tomates : ";
+                            cin>>P_HT;
+ cout<<"Entrer le nombre des kilos : ";
+                             cin>>nbr_kilo;
+  cout<<"Veuillez entrer le taux de TVA : ";
+                              cin>>TVA;
+   P_TTC=(1+TVA/100)*P_HT*nbr_kilo;
+ 
+    cout<<"Le prix TTC est : "<<P_TTC<<endl;
+return 0;
+}
