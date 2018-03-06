3e0a4d43cbf82e0626213120f6acb5586595abd6
CHAKFI Ahmed <36175107+CHAKFI@users.noreply.github.com>
2018-03-06 12:19:46 +0000

    test
    add file

---
+++ ex2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int la,lo,sf,pe;
+      cout<<"Veuillez entrer la largeur et la longeur"<<endl ;
+      cin>>la;
+      cin>>lo;
+              sf=la*lo;
+                pe=(la+lo)*2;
+       cout<<"La surface = " << sf <<endl;
+        cout<<"Le perimetre = " <<pe;
+   
+   
+   
+   return 0;
+   
+}
