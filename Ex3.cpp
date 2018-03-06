87ee6a869d32731bc4e73cc20fe08953ae04cef3
CHAKFI Ahmed <36175107+CHAKFI@users.noreply.github.com>
2018-03-06 21:11:05 +0000

    EXO
    CINQ ENTIERS

---
+++ Ex3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,s=0;
+ cout<<"Veuillez entrer les cinqs entiers "<<endl;
+   cin>>a;s=s+a;
+    cin>>a;s=s+a;
+     cin>>a;s=s+a;
+      cin>>a;s=s+a;
+       cin>>a;s=s+a;
+    
+	       s=s/5;
+
+cout<<"La moyenne de ces entiers est : "<<s<<endl;
+ 
+
+return 0;
+}
