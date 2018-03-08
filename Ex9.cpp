a6d1f9809e4b472ea16de9bbfaf934c117e86695
CHAKFI Ahmed <36175107+CHAKFI@users.noreply.github.com>
2018-03-08 21:49:12 +0000

    EXO
    la somme de dix_entiers (^___^)

---
+++ Ex9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int i,N,Som;
+	
+    Som=0;
+    
+    for(i=1;i<=10;i++)
+    {
+      cout<<"Veuillez entrer l entiers_"<<i<<endl;
+	   cin>>N;
+    	Som=Som+N;
+	}
+	cout<<Som;
+	
+	
+	
+	return 0;
+}
