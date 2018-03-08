8a59a3f20a21e6f11903687b1a85bf5e121708a5
CHAKFI Ahmed <36175107+CHAKFI@users.noreply.github.com>
2018-03-08 20:43:47 +0000

    exo
    Gagné !
    perdu ?

---
+++ Ex6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(int argc, char** argv) {
+	
+	int N;
+	
+	cout<<"Veuillez entrer un entiers"<<endl;
+	 cin>>N;
+	 
+	     if(N>=56 && N<=78)
+	      {
+	 	   cout<<"GAGNE"<<endl;
+		  }
+	     else
+	      {
+	 	   cout<<"PERDU";
+          } 	 	
+  
+  return 0;
+}
