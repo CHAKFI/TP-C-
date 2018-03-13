210d09ff8e5ac30bbf64f2cd28f946f057ea0b7d
CHAKFI Ahmed <36175107+CHAKFI@users.noreply.github.com>
2018-03-13 22:34:14 +0000

    EXO
    N !

---
+++ Ex19.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int i,n,f;
+	
+	cout<<"Veuillez entrer un entier"<<endl;
+	 cin>>n;
+	
+	   if(n<0)
+       {
+        cout<<" ERREUR"<<endl;
+       } 
+		  else
+           {
+	       f=1;
+	             for(i=1;i<=n;i++)
+	             {
+	              f=f*i;
+	             }
+	        cout<<"----> "<<n<<"!"<<" = "<<f;
+            }
+	return 0;
+}
