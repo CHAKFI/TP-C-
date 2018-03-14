3125c0b7666145849e05daa192d4339460103814
CHAKFI Ahmed <36175107+CHAKFI@users.noreply.github.com>
2018-03-14 00:10:12 +0000

    Exo
    (while + for + if ) (*_*)

---
+++ Ex11+.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int i,n,sm;
+	
+		do
+		{
+			cout<<"Veuillez entrer un entier (>=5)"<<endl;
+	cin>>n;
+		if(n>=5)
+	    {
+		
+	         for(i=5;i<=n;i++)
+	         {
+	         	sm=i*i*i;
+			 }
+			cout<<"La somme des cubes --->\t"<<sm; 
+	   }
+       }
+	   while (!(n>=5)); 
+	   
+	
+	return 0;
+}
