48e4502d8079d6e99ee6fc9ec03a95003ddba4f1
CHAKFI Ahmed <36175107+CHAKFI@users.noreply.github.com>
2018-03-14 00:42:52 +0000

    U(n)

---
+++ Ex12.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*  ANOUAR Abderrazzak  && CHAKFI Ahmed */
+
+int main(int argc, char** argv) {
+	
+	int i,u_n,N;
+	 u_n=3;
+	
+	cout<<"Veuillez entrer un entier"<<endl;
+	cin>>N;
+	      for(i=0;i>=N;i++)
+	      {
+	      	u_n= (3*u_n)+4;
+		  }
+	   cout<<endl<<"U(n) =\t"<<u_n;
+	
+	return 0;
+}
