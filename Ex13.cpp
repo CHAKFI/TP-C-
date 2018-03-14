c96a6d6e7e574b491bd59c45afcb81fd3a71d42f
CHAKFI Ahmed <36175107+CHAKFI@users.noreply.github.com>
2018-03-14 21:46:26 +0000

    tp_1
    ex u(n)

---
+++ Ex13.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int i,N,U_n,W_n,V_n;
+	
+	W_n=1;
+	V_n=1;
+ 
+cout<<"Veuiellez entrer un entier"<<endl;
+cin>>N;
+ 
+  U_n=1;
+ 
+for(i=2;i<=N;i++)
+        {
+        U_n=W_n+V_n;
+        W_n=V_n;
+        V_n=W_n;
+        }
+ 
+cout<<"U(n) =\t"<<U_n<<endl;
+ 
+	return 0;
+}
