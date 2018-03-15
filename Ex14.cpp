579838757244c0b29443dea3aedb50c43a9f4800
CHAKFI Ahmed <36175107+CHAKFI@users.noreply.github.com>
2018-03-15 20:29:14 +0000

    N+17

--- Ex14.cpp
+++ Ex14.cpp
@@ -13,8 +13,10 @@ int main(int argc, char** argv) {
 	cin>>N;
 
 	   p=N+17;
+	   if(N<0 || N>20) { cout<<endl<<"ERREUR"<<"\t\t" ;}
+	   
     }
-	while(N<=0 || N>=20);
+	while(N<0 || N>20);
 	  cout<<N<<" + 17 --->\t"<<p;
 	return 0;
 }
