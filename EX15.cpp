e0036e3ee5844be088f5053124dde5b330f36bea
ANOUAR AND CHAKFI <36175107+CHAKFI@users.noreply.github.com>
2018-04-11 22:49:08 +0100

    Ex15

---
+++ EX15.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/*CHAKFI Ahmed && ANOUAR Abderrazzak*/
+
+ 
+int main()
+{
+double n=0;
+int chx;
+ 
+ do
+    {
+    cout<<"Actuellement n = "<<n<<" \n 1:Ajouter 1 \n 2:Multiplier par 2 \n 3:Soustraire 4 \n 4:Quitter"<<endl;
+    cout<<"--->";cin>>chx;
+ 
+    switch(chx)
+        {
+        case 1: n++;break;
+        case 2: n=n*2; break;
+        case 3: n=n/4;break;
+        }
+}while(chx!=4);
+ 
+cout<<"La valeur finale de x vaut : "<<n<<endl;
+ 
+return 0;
+}
